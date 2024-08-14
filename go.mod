0b8f05c1da56eb449a1fe301d481cea4c54d0b87
Vera Juan <veracodeJuan@gmail.com>
2024-08-14 08:25:30 +0000

    more deps because go is very safe!

--- go.mod
+++ go.mod
@@ -2,4 +2,33 @@ module myapp
 
 go 1.23.0
 
-require golang.org/x/net v0.28.0 // indirect
+require (
+	github.com/docker/docker v23.0.3+incompatible
+	github.com/gorilla/websocket v1.4.2
+	github.com/prometheus/client_golang v1.19.1
+	golang.org/x/net v0.28.0
+)
+
+require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/distribution v2.8.3+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gotest.tools/v3 v3.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,21 +2,76 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/net/html"
+	"log"
+	"net/http"
 	"strings"
+
+	"golang.org/x/net/html"
+	"github.com/gorilla/websocket"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
 )
 
 func main() {
-	// Example HTML content
+	// Example 1: Using the html package from golang.org/x/net
 	htmlContent := `<html><body><h1>Hello, Go!</h1></body></html>`
 	doc, err := html.Parse(strings.NewReader(htmlContent))
 	if err != nil {
 		panic(err)
 	}
 	printNode(doc)
+
+	// Example 2: Using the websocket package
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		upgrader := websocket.Upgrader{}
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer conn.Close()
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Printf("Received message: %s\n", msg)
+		if err := conn.WriteMessage(msgType, msg); err != nil {
+			log.Println(err)
+			return
+		}
+	})
+	go func() {
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
+	// Example 3: Using Prometheus metrics
+	counter := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "example_counter",
+		Help: "An example of a counter in Prometheus",
+	})
+	prometheus.MustRegister(counter)
+	http.Handle("/metrics", promhttp.Handler())
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":9090", nil))
+	}()
+
+	// Example 4: Using Docker client
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatalf("Could not create Docker client: %s", err)
+	}
+	info, err := cli.Info(ctx)
+	if err != nil {
+		log.Fatalf("Could not retrieve Docker info: %s", err)
+	}
+	fmt.Printf("Docker info: %v\n", info)
 }
 
-// printNode recursively prints the node tree.
 func printNode(n *html.Node) {
 	if n.Type == html.ElementNode {
 		fmt.Printf("<%s>\n", n.Data)
